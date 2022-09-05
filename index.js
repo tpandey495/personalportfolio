7a63c6d0f88776b79a422407ceebe7d70273ed74
Tarun Pandey <you@example.com>
2022-09-05 18:49:52 +0530

    test

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 const express = require("express");
 const router = express.Router();
 const cors = require("cors");
+const path = require("path");
 const nodemailer = require("nodemailer");
-require('dotenv').config()
+require('dotenv').config();
 const PORT=process.env.PORT||5000;
 const app = express();
 
 //setting middleware
 app.use(cors());
 app.use(express.json());
-app.use("/", router);
+
 
 if ( process.env.NODE_ENV == "production"){
   app.use(express.static("client/build"));
-  const path = require("path");
-  app.get("*", (req, res) => {
+ app.get("*", (req, res) => {
   res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   })
 }
