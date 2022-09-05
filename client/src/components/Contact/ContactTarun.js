b71d3c71018acf6330ec7d21e6c779ea8a1891ba
Tarun Pandey <tpandey495@gmail.com>
2022-09-05 21:52:03 +0530

    Update ContactTarun.js

--- client/src/components/Contact/ContactTarun.js
+++ client/src/components/Contact/ContactTarun.js
@@ -18,7 +18,7 @@ const handleSubmit = async (e) => {
       sub:sub.value,
       message: message.value,
     };
-    let response = await fetch("http://localhost:5000/contact", {
+    let response = await fetch("https://tarunpandey.herokuapp.com/contact", {
       method: "POST",
       headers: {
         "Content-Type": "application/json;charset=utf-8",
