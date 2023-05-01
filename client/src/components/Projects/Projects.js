8a155a701b661892ed18819a1b5646aabba1a754
Tarun Pandey <tpandey495@gmail.com>
2023-05-01 16:29:47 +0530

    new project added

--- client/src/components/Projects/Projects.js
+++ client/src/components/Projects/Projects.js
@@ -2,9 +2,9 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
-import mstmovie from "../../Assets/Projects/mstmovie.png";
-import travel from "../../Assets/Projects/travel.png";
-import amazingui from "../../Assets/Projects/ama.png";
+import Capture from "../../Assets/Projects/Capture.png";
+import youtube from "../../Assets/Projects/youtube.png";
+import clothify from "../../Assets/Projects/clothify.png";
 
 function Projects() {
   return (
@@ -15,72 +15,37 @@ function Projects() {
           My Recent <strong className="purple">Works </strong>
         </h1>
         <p style={{ color: "white" }}>
-           Frontend  Projects
+            Projects
         </p>
-
-         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
-         {/* <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={chatify}
-              isBlog={false}
-              title="Chatify"
-              description="Personal Chat Room or Workspace to share resources and hangout with friends build with react.js, Material-UI, and Firebase. Have features which allows user for realtime messaging, image sharing as well as supports reactions on messages."
-              link=""
-            />
-          </Col>
-
+        <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={bitsOfCode}
+              imgPath={Capture}
               isBlog={false}
-              title="Bits-0f-C0de"
-              description="My personal blog page build with Next.js and Tailwind Css which takes the content from makdown files and renders it using Next.js. Supports dark mode and easy to write blogs using markdown."
-              link=""
+              title="Task Exhibitor"
+              description="Task Exhibitor is task and project management system designed to divide and
+              prioritize task in way that make individual and organization more productive and focused.."
+              link="https://github.com/praveen4star/Tracker"
             />
           </Col>
-
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={editor}
+              imgPath={youtube}
               isBlog={false}
-              title="Editor.io"
-              description="Online code and markdown editor build with react.js. Online Editor which supports html, css, and js code with instant view of website. Online markdown editor for building README file which supports GFM, Custom Html tags with toolbar and instant preview.Both the editor supports auto save of work using Local Storage"
-              link=""
-            />
-          </Col> */}
-
-
-<Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={travel}
-              isBlog={false}
-              title="Travel  Website"
-              description="Full-stack web application using JavaScript serving  Youtube API with React as the frontend."
-              link="https://github.com/tpandey495/Travel-Blogging-Website"
-            />
-          </Col>
-
-           <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={amazingui}
-              isBlog={false}
-              title="Amazing Ui"
-              description="Website containing  card , login as modal , Navbar , footer wave svg."
-                            link="https://amazinguiandux.netlify.app/"
+              title="Youtube Clone"
+              description="Hghly scalable YouTube clone app built  using modern web technologies, including React, Redux, and Tailwind CSS, and is powered by YouTube API v3. It offers an array of features, such as the ability to search and watch videos, live chat with other users, and discover related videos."
+              link="https://tarunyoutube.netlify.app/"
             />
           </Col> 
-
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={mstmovie}
+              imgPath={clothify}
               isBlog={false}
-              title="Blogging Website Template"
-              description="Blogging website Template with interactive Ui built using HTML , Css , Bootstrap and JavaScript."
-              link="https://github.com/tpandey495/-Blogging-Website-template"
+              title="Clothify"
+              description="Clothify a market place to buy and sell fancy clothes.It provides basic feature of ecommerce store."
+              link="https://clothifystore.onrender.com/"
             />
           </Col>
-
-          
         </Row>
       </Container>
     </Container>
